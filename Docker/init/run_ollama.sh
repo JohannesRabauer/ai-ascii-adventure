0367e9e83f51737398224b39e0860965bb392aa5
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-09-04 20:29:37 +0200

    Reverting back to llama3.2:3b model

--- Docker/init/run_ollama.sh
+++ Docker/init/run_ollama.sh
@@ -7,11 +7,12 @@ echo "Waiting for Ollama server to be active..."
 while [ -z "$(ollama list | grep 'NAME')" ]; do
   sleep 1
 done
+echo "Ollama is ready"
 
-echo "Ollama is ready, creating the model..."
+echo "Creating the model..."
 ollama create llama-rpg --file ./init/llama-rpg.modelfile
 
-echo "Ollama is ready, starting the model..."
+echo "Starting the model..."
 ollama run llama-rpg
 
 
